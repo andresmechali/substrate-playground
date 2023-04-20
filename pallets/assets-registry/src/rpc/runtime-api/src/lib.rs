ce54385628dafb4cda73134892f33247eb338ca0
Andrés Mechali <andresmechali@gmail.com>
2023-04-20 22:58:07 +0200

    fix missing rpc

--- pallets/assets-registry/src/rpc/runtime-api/src/lib.rs
+++ pallets/assets-registry/src/rpc/runtime-api/src/lib.rs
@@ -1,7 +1,8 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
 sp_api::decl_runtime_apis! {
-    pub trait AssetsRegistryApi {
-        fn get_value() -> u32;
-    }
+	#[api_version(2)]
+	pub trait AssetsRegistryApi {
+		fn get_value() -> u32;
+	}
 }
