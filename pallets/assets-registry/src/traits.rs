17f09da78831ec2ab65d6a0d3616b384d7c5dfc7
Andres Mechali <andresmechali@gmail.com>
2023-04-24 10:32:51 +0200

    fix build

--- pallets/assets-registry/src/traits.rs
+++ pallets/assets-registry/src/traits.rs
@@ -2,8 +2,7 @@ pub use crate::*;
 
 use frame_support::{inherent::Vec, pallet_prelude::*};
 
-#[derive(Encode, Decode, Default, PartialEq, Eq, TypeInfo, Clone)]
-#[cfg_attr(feature = "std", derive(Debug))]
+#[derive(Encode, Decode, Default, PartialEq, Eq, TypeInfo, Clone, Debug)]
 pub struct Asset<RegisteredAssetId, Balance> {
 	pub asset_id: RegisteredAssetId,
 	pub decimals: u8,
